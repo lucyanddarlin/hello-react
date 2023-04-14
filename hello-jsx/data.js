a66795de1bb2b565228950c9cc396823b9f6d4a4
lucyanddarlin <1194486835@qq.com>
2023-04-14 09:22:00 +0800

    add: 添加案例

---
+++ hello-jsx/data.js
@@ -0,0 +1,32 @@
+const books = [
+  {
+    id: 1,
+    name: 'a',
+    price: 10,
+    count: 1
+  },
+  {
+    id: 1,
+    name: 'a',
+    price: 10,
+    count: 1
+  },
+  {
+    id: 1,
+    name: 'a',
+    price: 10,
+    count: 1
+  },
+  {
+    id: 1,
+    name: 'a',
+    price: 10,
+    count: 1
+  },
+  {
+    id: 1,
+    name: 'a',
+    price: 10,
+    count: 1
+  },
+]
